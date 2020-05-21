ab8f753341db74c9c5bad5cf3e81c08d9542e5e6
Derek Webb <derek.webb@holbertonschool.com>
2020-05-21 19:06:08 +0000

    hbnb.3

--- models/base_model.py
+++ models/base_model.py
@@ -4,17 +4,19 @@ from datetime import datetime
 class BaseModel:
     def __init__(self):
         self.id = str(uuid4())
-        self.created_at = self.update_at = datetime.today().isoformat()
+        self.created_at = self.update_at = datetime.today()
 
     def __str__(self):
         className = self.__class__.__name__
         return "[{}] ({}) {}".format(className, self.id, self.__dict__)
 
     def save(self):
-        self.update_at = datetime.today().isoformat()
+        self.update_at = datetime.today()
 
     def to_dict(self):
         dictCopy = self.__dict__.copy()
+        dictCopy["created_at"] = self.created_at.isoformat()
+        dictCopy["updated_at"] = self.updated_at.isoformat()
         dictCopy["__class__"] = self.__class__.__name__
 
         return dictCopy
