f080d0ad7a34ab1c86df29a3f565050d971e98e8
Derek Webb <derek.webb@holbertonschool.com>
2020-05-21 23:02:56 +0000

    hbnb.4

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,17 @@ from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.today()
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key is not "__class__":
+                    if key is "created_at" or key is "updated_at":
+                        self.__dict__[key] = datetime(value, "%Y-%m-%dT%H:%M:%S%z")
+                    else:
+                        self.__dict__[key] = value
+        else:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.today()
 
     def __str__(self):
         className = self.__class__.__name__
