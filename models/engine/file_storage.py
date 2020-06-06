3dc638f743a78302ef85feeb403509a730174bcf
Derek Webb <derek.webb@holbertonschool.com>
2020-06-06 16:50:18 +0000

    Fixed key

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,7 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        self.__objects["{}.{}".format(obj.__name__, obj.id)] = obj
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         jsonText = json.dumps(self.__objects)
